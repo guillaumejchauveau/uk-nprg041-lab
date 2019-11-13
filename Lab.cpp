f8eceb0c4c251d299db3382a5ff456b4e8e98d64
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2019-11-13 22:51:56 +0100

    Lab 7

--- Lab.cpp
+++ Lab.cpp
@@ -1,9 +1,60 @@
 #include <iostream>
-#include "Str.h"
+#include <list>
+#include <memory>
+
+class Animal {
+ public:
+  virtual ~Animal() {};
+  virtual void sound() = 0;
+};
+
+class Wolf : public Animal {
+  int size;
+ public:
+  Wolf(int size) : size{size} {}
+  ~Wolf() override {
+    std::cout << "lol" << std::endl;
+  }
+  void sound() override {
+    std::cout << "Howl!" << this->size << std::endl;
+  }
+};
+
+class Chicken : public Animal {
+  int tastiness;
+ public:
+  Chicken(int tastiness) : tastiness{tastiness} {}
+  void sound() {
+    std::cout << "kdjsqiodjqs" << this->tastiness << std::endl;
+  }
+};
+
+class Kangaroo : public Animal {
+  std::unique_ptr<Animal> passenger;
+ public:
+  Kangaroo(std::unique_ptr<Animal> passenger) : passenger{move(passenger)} {}
+  void sound() {
+    std::cout << "Boing ";
+    passenger->sound();
+  }
+};
+
+class Shark : public Animal {
+  std::list<std::unique_ptr<Animal>> eaten;
+};
 
 int main() {
-  Str a = Str("lol");
-  std::cout << a << std::endl;
-
+  std::list<std::unique_ptr<Animal>> zoo;
+  zoo.push_back(std::make_unique<Wolf>(Wolf(2)));
+  zoo.push_back(std::make_unique<Chicken>(Chicken(42)));
+  zoo.push_back(std::make_unique<Kangaroo>(std::make_unique<Kangaroo>(std::make_unique<Chicken>(8))));
+  int chicken_count = 0;
+  for (auto &a : zoo) {
+    a->sound();
+    if (dynamic_cast<Chicken *>(a.get())) {
+      chicken_count++;
+    }
+  }
+  std::cout << chicken_count << std::endl;
   return 0;
 }
